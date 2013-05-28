c17aeed8713d6ca9137e23df6d67e1d6068c36ef
roypeled <roy.peled@conduit.com>
2013-05-28 18:45:29 +0300

    added support for slow loading iframes

--- filter.js
+++ filter.js
@@ -4,5 +4,14 @@ chrome.tabs.onUpdated.addListener(function(tabID, change, tab){
             code: '#imgBeforeLike, .fb-like-div {display:none !important;} .mainDiv{ overflow:auto !important};',
             allFrames: true
         })
+
+        // Sometimes it takes time for the iframe to load, so I'll add a delay and try again
+
+        setTimeout(function(){
+            chrome.tabs.insertCSS(tabID, {
+                code: '#imgBeforeLike, .fb-like-div {display:none !important;} .mainDiv{ overflow:auto !important};',
+                allFrames: true
+            })
+        }, 5000);
     }
 })
